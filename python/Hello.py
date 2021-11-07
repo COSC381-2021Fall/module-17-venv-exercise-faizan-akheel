c224e16a2bd69f838a9fbaecb0dc4f3b010a6b94
faizan-akheel <fakheel@emich.edu>
2021-11-07 18:39:04 +0000

    Pushed Project

---
+++ python/Hello.py
@@ -0,0 +1,7 @@
+import emoji
+import pyjokes
+from PyDictionary import PyDictionary
+
+print(emoji.emojize('Python is :thumbs_up:'))
+print(pyjokes.get_joke())
+print(PyDictionary().meaning("hello"))
